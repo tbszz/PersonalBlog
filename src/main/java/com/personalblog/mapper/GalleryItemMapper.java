a8a3127aaae66b2c080136ae92d9cd669a0e06b8
tbszz <17814612130@163.com>
2026-01-03 22:28:19 +0800

    Cleanup: Remove legacy backend code, Docker config, and local uploads

--- src/main/java/com/personalblog/mapper/GalleryItemMapper.java
+++
@@ -1,9 +0,0 @@
-package com.personalblog.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.personalblog.domain.GalleryItem;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface GalleryItemMapper extends BaseMapper<GalleryItem> {
-}
--- src/main/java/com/personalblog/mapper/UserMapper.java
+++
@@ -1,9 +0,0 @@
-package com.personalblog.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.personalblog.domain.User;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface UserMapper extends BaseMapper<User> {
-}
